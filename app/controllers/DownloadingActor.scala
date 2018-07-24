320a75dc0bb5bdc03a8f08bde95d27b9a4f76180
sondv <sondv>
2018-07-24 10:55:57 +0700

    Fixed unable to download due to exceeded file name length

--- app/controllers/DownloadingActor.scala
+++ app/controllers/DownloadingActor.scala
@@ -17,23 +17,26 @@ class DownloadingActor(out: ActorRef) extends Actor {
 
   def download(link: String, logger: ActorRef) = {
     val captureLog = (log: String) => logger ! log
+    val getFileName: String => Option[String] = s => s.split("/")
+      .lastOption
+      .map(_.replaceAll("\\s+", ""))
+      .map(filename => if (filename.length < 50) filename else "download")
 
     "which wget".lineStream
       .headOption
       .foreach(cmd => {
         Try {
-         val url = new URI(link).toURL.toString
-         url.split("/").lastOption
-          .map(_.replaceAll("\\s+", ""))
-          .foreach { filename =>
-            println(s"Saving to ${Conf.tmpDir}/$filename")
-            val command = s"$cmd --no-check-certificate -t 3 -c -O ${Conf.tmpDir}${File.separator}$filename $link"
-            println("Executing: " + command)
-            val exitCode = command ! ProcessLogger(captureLog, captureLog)
+          val url = new URI(link).toURL.toString
+          getFileName(url)
+            .foreach { filename =>
+              println(s"Saving to ${Conf.tmpDir}/$filename")
+              val command = s"$cmd --no-check-certificate -t 3 -c -O ${Conf.tmpDir}${File.separator}$filename $link"
+              println("Executing: " + command)
+              val exitCode = command ! ProcessLogger(captureLog, captureLog)
 
-            if (exitCode == 0) captureLog(s"Downloaded:$filename")
-            else captureLog(s"Unable to download from $link")
-          }
+              if (exitCode == 0) captureLog(s"Downloaded:$filename")
+              else captureLog(s"Unable to download from $link")
+            }
         }.getOrElse {
           captureLog("Invalid URL: " + link)
         }
